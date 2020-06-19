5138a3ec5fefacd1ea27e3090a3efae3575b5d35
thewhite <thesmallwhiteme@163.com>
2020-06-19 08:31:10 +0800

    flex

--- web/src/views/index.vue
+++ web/src/views/index.vue
@@ -1,8 +1,7 @@
 <template>
     <div>
         <header-nav></header-nav>
-        <div class="main-p">
-            <a-menu class="menus"
+        <a-menu class="menus main-p"
                     mode="horizontal">
                 <a-menu-item key="mail"> <a-icon type="mail" />Navigation One </a-menu-item>
                 <a-menu-item key="app"> <a-icon type="appstore" />Navigation Two </a-menu-item>
@@ -28,66 +27,78 @@
                     </a-menu-item-group>
                 </a-sub-menu>
                 <a-menu-item key="alipay">
-                    <a href="https://antdv.com" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
+                    Navigation Four - Link
                 </a-menu-item>
             </a-menu>
-            <div style="width: 188px;text-align: left;">
-                <a-button type="primary"
-                          style="margin-bottom: 16px;position: fixed;left:10%"
-                          @click="toggleCollapsed">
-                    <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
-                </a-button>
-                <a-menu
-                        :default-selected-keys="['1']"
-                        :default-open-keys="['sub1']"
-                        mode="inline"
-                        theme="dark"
-                        :inline-collapsed="collapsed">
-                    <a-menu-item key="1">
-                        <a-icon type="pie-chart" />
-                        <span>Option 1</span>
+        <div class="box main-m">
+            <a-menu class="menus-vertical"
+                    :default-selected-keys="['1']"
+                    :default-open-keys="['sub1']"
+                    mode="inline"
+                    :inline-collapsed="collapsed">
+                <a-menu-item key="1">
+                    <a-icon type="pie-chart" />
+                    <span>Option 1</span>
+                </a-menu-item>
+                <a-menu-item key="2">
+                    <a-icon type="desktop" />
+                    <span>Option 2</span>
+                </a-menu-item>
+                <a-menu-item key="3">
+                    <a-icon type="inbox" />
+                    <span>Option 3</span>
+                </a-menu-item>
+                <a-sub-menu key="sub1">
+                    <span slot="title"><a-icon type="mail" /><span>Navigation One</span></span>
+                    <a-menu-item key="5">
+                        Option 5
                     </a-menu-item>
-                    <a-menu-item key="2">
-                        <a-icon type="desktop" />
-                        <span>Option 2</span>
+                    <a-menu-item key="6">
+                        Option 6
                     </a-menu-item>
-                    <a-menu-item key="3">
-                        <a-icon type="inbox" />
-                        <span>Option 3</span>
+                    <a-menu-item key="7">
+                        Option 7
                     </a-menu-item>
-                    <a-sub-menu key="sub1">
-                        <span slot="title"><a-icon type="mail" /><span>Navigation One</span></span>
-                        <a-menu-item key="5">
-                            Option 5
+                    <a-menu-item key="8">
+                        Option 8
+                    </a-menu-item>
+                </a-sub-menu>
+                <a-sub-menu key="sub2">
+                    <span slot="title"><a-icon type="appstore" /><span>Navigation Two</span></span>
+                    <a-menu-item key="9">
+                        Option 9
+                    </a-menu-item>
+                    <a-menu-item key="10">
+                        Option 10
+                    </a-menu-item>
+                    <a-sub-menu key="sub3" title="Submenu">
+                        <a-menu-item key="11">
+                            Option 11
                         </a-menu-item>
-                        <a-menu-item key="6">
-                            Option 6
-                        </a-menu-item>
-                        <a-menu-item key="7">
-                            Option 7
-                        </a-menu-item>
-                        <a-menu-item key="8">
-                            Option 8
+                        <a-menu-item key="12">
+                            Option 12
                         </a-menu-item>
                     </a-sub-menu>
-                    <a-sub-menu key="sub2">
-                        <span slot="title"><a-icon type="appstore" /><span>Navigation Two</span></span>
-                        <a-menu-item key="9">
-                            Option 9
-                        </a-menu-item>
-                        <a-menu-item key="10">
-                            Option 10
-                        </a-menu-item>
-                        <a-sub-menu key="sub3" title="Submenu">
-                            <a-menu-item key="11">
-                                Option 11
-                            </a-menu-item>
-                            <a-menu-item key="12">
-                                Option 12
-                            </a-menu-item>
-                        </a-sub-menu>
-                    </a-sub-menu>
-                </a-menu>
+                </a-sub-menu>
+            </a-menu>
+            <div class="left-container">
+                <div class="banner">
+                    <transition name="fade">
+                        <div v-show="time == 1">
+                            <img :src="img1">
+                        </div>
+                    </transition>
+                    <transition name="fade">
+                        <div v-show="time == 2">
+                            <img :src="img2">
+                        </div>
+                    </transition>
+                    <transition name="fade">
+                        <div v-show="time == 3">
+                            <img :src="img3">
+                        </div>
+                    </transition>
+                </div>
             </div>
         </div>
     </div>
@@ -102,24 +113,38 @@
         },
         data() {
             return {
-                collapsed: false,
+                collapsed:false,
+                img1:require('../assets/banner1.jpg'),
+                img2:require('../assets/banner2.jpg'),
+                img3:require('../assets/banner3.jpg'),
+                time:1,
             };
         },
-        methods: {
-            toggleCollapsed() {
-                this.collapsed = !this.collapsed;
-            },
-        },
+        mounted() {
+            let _this = this
+            setInterval(function () {
+                _this.time++
+                if (_this.time == 4){
+                    _this.time = 1
+                }
+            },1500)
+        }
     }
 </script>
 
 <style scoped>
+    .box{
+        display: -webkit-flex; /* Safari */
+        display: flex;
+    }
     .menus {
         text-align: left;
         background: #0c273c;
         color: #FFFFFF;
         font-size: 14px;
         font-weight: bold;
+        height: 48px;
+        width: 100%;
     }
     .menus li:nth-child(2){
         background: #ff5b5b;
@@ -128,4 +153,62 @@
     .menus>.ant-menu-item-active{
         color: #FFFFFF !important;
     }
+
+    .menus-vertical{
+        width: 188px;
+        text-align: left;
+        border: none;
+    }
+    .menus-vertical li {
+        border:1px dashed #ededed !important;
+        border-top: none !important;
+        margin: 0 !important;
+    }
+    .menus-vertical .ant-menu-item-selected {
+        background: #f8f8f8 !important;
+    }
+    .ant-menu-sub li {
+        border-right: none !important;
+        border-left: none !important;
+    }
+    .ant-menu-sub li:last-child{
+        border-bottom: none !important;
+    }
+    .ant-menu-sub li:first-child{
+        border-top:1px dashed #ededed !important;
+    }
+
+    .left-container{
+        flex:1;
+        margin:15px 0 0 15px;
+    }
+    .banner{
+        height: 180px;
+        width: 100%;
+        overflow: hidden;
+        position: relative;
+    }
+    .banner div{
+        height: 100%;
+        width: 100%;
+        position: absolute;
+    }
+
+
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: all 1s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+    }
+
+    .fade-enter {
+        transform: translateX(200px);
+    }
+    .fade-leave-active {
+        transform: translateX(-200px);
+    }
+
+
 </style>
--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        <h3>login</h3>
+    <div id="father">
+        <div class="child1"></div>
+        <div class="child2"></div>
+        <div class="child3"></div>
     </div>
 </template>
 
@@ -11,5 +13,23 @@
 </script>
 
 <style scoped>
-
+    #father{
+        height: 100px;
+        background-color: #ff0;
+        display:flex;
+    }
+    div.child1{
+        width: 100px;
+        background-color: #369;
+    }
+    div.child2{
+        width: 100px;
+        background-color: #963;
+        flex:1
+        /*  这里的宽度是不起作用的 由于flex:1   */
+    }
+    div.child3{
+        width: 100px;
+        background-color: #339;
+    }
 </style>
